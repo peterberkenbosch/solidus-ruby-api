94761ad4ece77044e6c8f955ec0d6420daebe38a
Peter Berkenbosch <info@peterberkenbosch.nl>
2022-07-20 17:34:11 -0300

    Use https in tests endpoint

--- test/solidus_api/test_client.rb
+++ test/solidus_api/test_client.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class TestClient < Minitest::Test
   def test_api_key
-    client = SolidusApi::Client.new api_key: "key", api_endpoint: "localhost:4000/api"
+    client = SolidusApi::Client.new api_key: "key", api_endpoint: "https://localhost:4000/api"
     assert_equal "key", client.api_key
-    assert_equal "localhost:4000/api", client.api_endpoint
+    assert_equal "https://localhost:4000/api", client.api_endpoint
   end
 end
